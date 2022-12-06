6412afb6d27b93c5c56237b9b0fbc4b7513a19c4
btran946 <105252771+btran946@users.noreply.github.com>
2022-12-06 12:55:27 -0800

    if implemented, server can talk to api

--- server/Controllers/cityController.js
+++ server/Controllers/cityController.js
@@ -4,8 +4,9 @@ const cityController = {};
 cityController.getDetails = async(req, res, next) => {
   try {
     console.log('in cityController')  
-    const data = await fetch('https://api.teleport.org/api/urban_areas/string%3Ateleport%3A9q8yy/')
-    console.log(data)
+    const data = await fetch('https://api.teleport.org/api/urban_areas/slug:san-diego/details')
+    const res = await data.json();
+    console.log(res.categories);
     return next();
 }
 catch (err){
