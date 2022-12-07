9621241b1d29a502eaacc70b747fc50bb9cbe3e7
RussoDavid <dr2378@gmail.com>
2022-12-06 21:37:46 -0500

    data names and scores

--- client/components/Citybox.jsx
+++ client/components/Citybox.jsx
@@ -10,6 +10,7 @@ export default function Citybox(){
   const [rightBoxData, seRightBoxData] = useState();
   const [items, setItems] = useState('')
   const [cities, setCities] = useState('');
+  const [data, setData] = useState('');
 
   useEffect(() => {
   console.log('inside use effect')
@@ -47,10 +48,27 @@ export default function Citybox(){
     }
   }
 
+  function displayScores(scoreData) {
+    console.log('in display scores', scoreData);
+    let arr = [];
+    console.log('categories', scoreData.categories);
+    for (let i = 0; i < scoreData.categories.length; i++) {
+      arr.push(
+      <div className="scoreContainer" key={'scoreContainerKey' + i}>
+      <p className="scoreLabel" key={'Labelkey' + i}>{scoreData.categories[i].name}</p>
+      <p className='scores' key={'Scorekey' + i}>{Math.floor(scoreData.categories[i].score_out_of_10)}</p>
+      </div>
+      )
+    }
+    console.log('ARR', arr);
+    setData(arr);
+    console.log('DATA', data)
+    return;
+  }
+
   function submitButtonClick() {
     const input = document.getElementById('myInput');
     const newCity = input.value.replaceAll(' ', '-').toLowerCase();
-    console.log('DASH', newCity)
     axios({
     method: 'post',
     url: '/getDetails',
@@ -58,21 +76,32 @@ export default function Citybox(){
       city: newCity,
     }})
     .then(res => {
-    console.log(res);
+    console.log('RES', res);
+    // setData(res.data);
+    displayScores(res.data);
+    input.value = '';
   })
   .catch(function (error) {
-    console.log('Error here:', error);
+     console.log('Error here:', error);
   })
   }
   
   return(
-      <div id="CityBoxContainer">
-        <h3>Enter City: </h3>
-        <div className="autoComplete">
-          <input onChange={(e) => handleChange(e)} id="myInput" type="text" name="myCity" placeholder="city"></input>
-        </div>
-        <input type="submit" onClick={(e) => submitButtonClick(e)}></input>
+    <div className="container">
+        <div id="cityBoxSearch">
+            <div className="autoComplete">
+            <h3>Enter City: </h3>
+            <input onChange={(e) => handleChange(e)} id="myInput" type="text" name="myCity" placeholder="city"></input>
+            </div>
+            <input type="submit" onClick={(e) => submitButtonClick(e)}></input>
         <div className="autoComplete-list">{items}</div>
+        </div>
+      <div id='dataState'>
+          {data}
       </div>
+     
+    </div>
+    
+      
   )
   }
